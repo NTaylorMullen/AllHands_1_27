667d0c96ebc9c3d15735be308fecabcbaff9f7ee
N. Taylor Mullen <nimullen@microsoft.com>
2020-01-23 13:08:12 -0800

    Add base content for FAR, goto def, goto impl, colorization, CodeLens, and rename.

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace AllHands_1_27
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<WeatherForecastService, DefaultWeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
